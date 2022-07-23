305880eafb096a2063863d3343a705661587e04a
myeongu <ausrndi@gmail.com>
2022-07-23 15:19:03 +0900

    리액트 엘리먼트

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
+import React from 'react';
 import { Button } from 'antd';
 import 'App.css';
 
+class Component1 extends React.Component {
+  state = {
+    value: this.props.initialValue,
+  };
+  
+  onClick = () => {
+    const { value } = this.state
+    this.setState({ value: value + 1 })
+  }
+
+  render() {
+    const { value } = this.state; // 현재의 상태값 참조
+    return (
+      <div>
+        Counter: { value }
+        <Button onClick={this.onClick}>+1</Button>
+      </div> // jsx 문법
+    )
+  }
+}
+
+class FruitComponent extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>좋아하는 과일</h1>
+        <ul>
+          {
+            this.props.fruits.map((name, idx) => 
+              <li key={idx}>{name}</li>
+            )
+          }
+        </ul>
+      </div>
+    )
+  }
+}
+
 function App() {
+  const fruits = ["바나나", "사과", "딸기"]
   return (
     <div>
-      <button>
-        hello react.
-      </button>
-      <Button type="primary" onClick={ () => console.log("hello!") }>
-        Hello, Antd.
-      </Button>
+      <Component1 initialValue={10} />
+      <FruitComponent fruits={fruits} />
     </div>
   );
 }
