d4d67efa9ab5ada9bad429ff322605e31aabc8ae
myeongu <ausrndi@gmail.com>
2022-07-24 20:40:39 +0900

    커스텀 Hook 만들기

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import App2 from 'App2';
 import App3 from 'App3'
+import App4 from 'App4'
 import reportWebVitals from './reportWebVitals';
 import TodoList from 'TodoList';
 import EpisodeList from "EpisodeList";
@@ -11,7 +12,7 @@ import EpisodeList from "EpisodeList";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App3 />
+    <App4 />
     {/* <EpisodeList /> */}
   </React.StrictMode>
 );
