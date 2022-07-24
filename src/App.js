7b5b99e2b7e175de51575614cd716b14debabe82
myeongu <ausrndi@gmail.com>
2022-07-24 15:23:08 +0900

    CSS를 적용하는 다양한 방법

--- src/App.js
+++ src/App.js
@@ -5,28 +5,15 @@ import 'App.css';
 
 
 class App extends React.Component {
-  state = {
-    myquery: "",
-    language: "",
-  }
-
-  onChange = (e) => {
-    const {name, value} = e.target;
-    this.setState({
-      [name]: value,
-    })
-  }
-
+  
   render() {
     return (
-      <>
-        <Counter onClick={() => console.log("clicked!")} />
-        <input name='myquery' onChange={this.onChange} />
-        <input name='language' onChange={this.onChange} />
-        <hr/>
-        {JSON.stringify(this.state)}
-      </>
-    )
+      <div>
+        <Counter />
+        <Counter color={"green"} />
+        <Counter color="blue" />
+      </div>
+    );
   }
 }
 
--- src/Counter.js
+++ src/Counter.js
@@ -2,20 +2,55 @@ import React from "react";
 import PropTypes from "prop-types";
 
 class Counter extends React.Component {
-    static propTypes = {
-        onClick: PropTypes.func,
+    static defaultProps = {
+        color: "red",
     }
-    state = {
 
+    static propsTypes = {
+        color: PropTypes.string,
+    }
+
+    state = {
+        color: this.props.color,
+        value: 0,
+    }
+
+    onClick = () => {
+        // this.setState({ value : this.state.value + 1 })
+        this.setState(({ value: prevValue }) => ({ // value 변수를 prevValue이름으로 사용!
+            value: prevValue + 1,
+        }))
+    }
+
+    onContextMenu = (e) => {
+        e.preventDefault();
+        this.setState(({ value: prevValue }) => ({ // value 변수를 prevValue이름으로 사용!
+            value: (prevValue >= 1 ? prevValue - 1 : 0),
+        }))
     }
 
     render() {
+        const { color, value } = this.state;
         return (
-            <div onClick={this.props.onClick}>
-                Counter
+            <div onClick={this.onClick} 
+                 onContextMenu={this.onContextMenu}
+                 style={ {...style, backgroundColor:color} }>
+                {value}
             </div>
         )
     }
+}
+
+const style = {
+    width: '100px',
+    height: '100px',
+    display: 'inline-block',
+    textAlign: 'center',
+    borderRadius: '50px',
+    lineHeight: '100px',
+    userSelect: 'none',
+    fontSize: '3rem',
+    margin: '1rem',
 }
 
 export default Counter;
